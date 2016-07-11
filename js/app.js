35bca8a317ab8b434965b052c69372e0b4a7f8d2
Agnieszka Czaplicka <agnieszka0czaplicka@gmail.com>
2016-07-11 14:53:30 +0200

    projekt_v1
    grid + 1 strona

---
+++ js/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+//    
+//var $navMenu = $(".navbar");
+//var $buttonNav = $(".menu_btn");
+//	
+//	$buttonNav.on("click", function(event){
+//		$navMenu.show("slow");
+//		$(this).toggle();
+//	})
+
+//	$(window).on("resize", function(){
+//		if (window.matchMedia("(min-width: 600px)")) {
+//			$navMenu.css("display", "flex");
+//		}
+//	});
+	
+});
