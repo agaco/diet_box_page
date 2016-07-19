14441dce990a3f98ed3e54c83d4c0396d6a39f3b
Agnieszka Czaplicka <agnieszka0czaplicka@gmail.com>
2016-07-19 21:29:32 +0200

    dodane podstrony

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ var burgerButton = $('.menu-btn');
 var menuNav = $('.menu-bar');
 var menuA = menuNav.find("a")
 var body = $("html, body");	
+
 	
 //PAGE SCROLLING	
 	//body.stop().animate({scrollTop: 0}, 500);
@@ -34,16 +35,16 @@ var body = $("html, body");
     })
  
     $(window).on('resize load', function(event) {
-        if (window.matchMedia("(min-width: 640px)").matches) {
+        if (window.matchMedia("(min-width: 850px)").matches) {
 			menuNav.addClass("menu-for-desktop");
 		    menuNav.removeClass("menu-for-mobile");
-        } else if (window.matchMedia("(max-width: 639px)").matches) {
+        } else if (window.matchMedia("(max-width: 849px)").matches) {
 			menuNav.removeClass("menu-for-mobile");
 		    menuNav.removeClass("menu-for-desktop");
         }
     })
 	menuA.on('click', function(event) {
-         if (window.matchMedia("(max-width: 639px)").matches) {
+         if (window.matchMedia("(max-width: 850px)").matches) {
 			menuNav.removeClass("menu-for-mobile");
         }
     })
@@ -55,20 +56,32 @@ var body = $("html, body");
 		$(".logging").slideToggle("fast");
 	})
 	
+	//Kafelki - sekcja programy	
+	$(".tile").on("click", function(event){
+		console.log("test");
+		$(this).hide().next().fadeToggle("fast")
+	//		.animate({
+	//		opacity: 1,
+	//	},2000)	
+	})	
+
+	$(".open-prog").on("click", function(event){
+		$(this).hide().prev().show()
+	})
+
+	//BUTTON_UP
+	var topPosition = $("#wrap-0").position().top;
+	
+	$(".scroll-btn-up").on("click", function(event){
+		body.animate({scrollTop: topPosition}, 1500); 
+	})
 	
 	
-//Kafelki - sekcja programy	
-$(".tile").on("click", function(event){
-	console.log("test");
-	$(this).hide().next().fadeToggle("fast")
-//		.animate({
-//		opacity: 1,
-//	},2000)	
-})	
-
-$(".open-prog").on("click", function(event){
-	$(this).hide().prev().show()
-})
-
-
+	
+	//SCROLL
+	$(function() {
+		$.scrollify({
+			section : ".scroll-plug",
+		});
+	});
 });
